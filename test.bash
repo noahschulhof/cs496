c905d7ef341597f4502f9ff02c9ad797748bf71f
Noah Schulhof <noahschulhof2026@u.northwestern.edu>
2026-05-29 19:52:10 -0500

    update for quest

--- test.bash
+++ test.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=e32998        # groups command on Quest to find out
 #SBATCH --partition=gengpu      # leave as is
-#SBATCH --gres=gpu:8            # number of GPUs (suggest 4-8)
-#SBATCH --time=12:00:00            # suggest 12 hrs just in case
+#SBATCH --gres=gpu:1            # number of GPUs (suggest 4-8)
+#SBATCH --time=00:10:00            # suggest 12 hrs just in case
 #SBATCH --nodes=1               # recommended is 1 node
-#SBATCH --ntasks=4              # suggest 4
-#SBATCH --mem=128G               # suggest 64GB
-#SBATCH --output=experiment.out       # output file - rename
-#SBATCH --error=experiment.err        # error file - rename
-#SBATCH --job-name=experiment         # job name - rename
+#SBATCH --ntasks=1              # suggest 4
+#SBATCH --mem=8G               # suggest 64GB
+#SBATCH --output=test.out       # output file - rename
+#SBATCH --error=test.err        # error file - rename
+#SBATCH --job-name=test         # job name - rename
 #SBATCH --mail-type=ALL         # send email at begin and end of job or failure
 #SBATCH --mail-user=nschulhof@u.northwestern.edu       # your NU email address
 
